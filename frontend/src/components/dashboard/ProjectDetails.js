a0ef03c43ec4f8cdb2d3f70eb95b33ed5662be26
Madhukunchalaa <madhu@smartsolutionsdigi.com>
2025-06-27 11:29:26 +0530

    some changes

--- frontend/src/components/dashboard/ProjectDetails.js
+++ frontend/src/components/dashboard/ProjectDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { managerService } from '../../services/api';
+import ProjectSteps from '../common/ProjectSteps';
 import { 
   ArrowLeft, 
   Calendar, 
