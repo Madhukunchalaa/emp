201dd11742f6035efdc4f27e0b86137f3e709f17
Madhukunchalaa <madhu@smartsolutionsdigi.com>
2025-07-07 08:11:15 +0530

    chat

--- frontend/src/components/common/Chat.js
+++ frontend/src/components/common/Chat.js
@@ -4,7 +4,7 @@ import { ChatNotificationContext } from './Navbar';
 import { chatService } from '../../services/api';
 
 // Socket URL - should be configurable for deployment
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://emp-1-rgfq.onrender.com/api';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'https://emp-1-rgfq.onrender.com';
 
 const socket = io(SOCKET_URL, { transports: ['websocket'] });
 
