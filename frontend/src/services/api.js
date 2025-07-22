528db441917458f8dae613b9337d06a5612e1b30
Madhukunchalaa <madhu@smartsolutionsdigi.com>
2025-07-22 19:13:27 +0530

    chnages

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const API_URL = 'http://localhost:5000/api';
-const API_URL = 'https://emp-1-rgfq.onrender.com/api';
+const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'https://emp-1-rgfq.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
